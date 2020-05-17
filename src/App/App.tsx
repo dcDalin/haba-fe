ec3706db1fdac37a60140052dc183521e0965e50
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2020-05-17 12:58:22 +0300

    add google analytics

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import history from '../history';
 import AuthState from '../context/AuthContext/authState';
 import ActiveNavState from '../context/ActiveNavContext/activeNavState';
 import ResponsiveContainer from '../components/ResponsiveContainer';
 import Routes from './Routes';
+
+ReactGA.initialize('UA-166917981-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App: React.FC = () => {
   return (
