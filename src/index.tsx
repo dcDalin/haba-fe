7f7149c29975119c5df36c73c509be55072cde34
Dalin  Oluoch <dc_dalin@Dalins-MacBook-Pro.local>
2020-05-21 01:26:17 +0300

    Add global image contain

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import ApolloProvider from './ApolloProvider';
 import 'semantic-ui-css/semantic.min.css';
+import './index.css';
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
